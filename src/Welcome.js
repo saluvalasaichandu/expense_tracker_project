bd91a6464cc2497fc37df5d954048514d42e63f2
saluvalasaichandu <saluvalasaichand@gmail.com>
2023-12-06 16:26:13 +0530

    Updated_profile_page

--- src/Welcome.js
+++ src/Welcome.js
@@ -1,8 +1,43 @@
 import React from 'react'
 
 const Welcome = () => {
+    const idToken = localStorage.getItem("tokenET");
+    const profileHandler=()=>{
+        fetch("https://identitytoolkit.googleapis.com/v1/accounts:update?key=AIzaSyBGZHVIOrHGdhqLgr3AkPQoFPv2UZMymXA",{
+            method:'POST',
+            body:JSON.stringify({
+                idToken:idToken,
+                displayName:"saichandu",
+                returnSecureToken:true,
+            }),
+            headers:{
+                "Content-Type": "application/json",
+            }
+        })
+        .then((res)=>{console.log(res.data)})
+        .catch((err)=>console.log(err))
+        
+    }
+    const formhandler=()=>{
+        return(<p>detailsUpdated</p>)
+    }
   return (
-    <div>Welcome to Expense Tracker Project</div>
+    <div>
+         <p> Welcome to ExpenseTracker Page!!</p>
+         <p>Your Profile is incomplete <button onClick={profileHandler}>Complete Your Profile</button></p>
+         <form onSubmit={formhandler}>
+         <div><label>Name</label>
+         <input type='text '></input>
+         </div>
+         <div>
+            <label> Mobile</label>
+            <input type='text'></input>
+         </div>
+         <div>
+            <button type='submit'>Submit</button>
+         </div>
+         </form>
+    </div>
   )
 }
 
